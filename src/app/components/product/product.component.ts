4c0caf79bfd99abe489dcecab97ba1a0a70eb46e
Halis Uyanık <halisuyanik@outlook.com>
2022-12-06 02:05:08 +0300

    pipes

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -12,9 +14,16 @@ export class ProductComponent implements OnInit{
 
   products:Product[] = [];
 
-  constructor(private productService:ProductService, private activatedRoute:ActivatedRoute){}
+  constructor(
+    private productService:ProductService, 
+    private activatedRoute:ActivatedRoute, 
+    private toastrService:ToastrService,
+    private cartService:CartService,
+    ){}
 
   dataLoaded=false;
+
+  filterText="";
 
   ngOnInit(): void{
     this.activatedRoute.params.subscribe(params=>{
@@ -34,6 +43,11 @@ export class ProductComponent implements OnInit{
       this.products=response.data;
       this.dataLoaded=true;
     })
+  }
+  addToCart(product:Product){
+    this.toastrService.success("added to cart", product.productName);
+    this.cartService.addToCart(product);
+    
   }
 
 }
